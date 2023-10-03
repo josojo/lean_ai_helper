13089a9d1af53020a88f2d558bcb79c69e099ab9
josojo <josojo@hotmail.de>
2023-10-03 23:55:48 +0200

    Documentation of unregulararity (#17)
    The test exhibits non-standard behavior. It shows that the intermediate state in a proof is not fully supported. But we can always get there by submitting all tactics of a proof step by step

---
+++ tests/anomalities/test_gym_with_single_tactic.py
@@ -0,0 +1,47 @@
+import pytest
+import os
+from loguru import logger
+
+from src.interaction.gym import Gym, ProofFinished
+from src.mwe import Mwe
+from src.trace.trace import Tracer
+
+
+def test_example_1() -> None:
+    """Test the example from https://leanprover-community.github.io/mwe.html."""
+    code = ""
+    # Get the absolute path to the directory of the current script
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+
+    # Join the script directory with the relative path to the file
+    file_path = os.path.join(script_dir, "../data/Mathlib.Algebra.Algebra.Basic.lean")
+
+    # Open the file using the absolute path
+    file = open(file_path, "r", encoding="utf-8")
+    code = file.read()
+    file.close()
+    mwe = Mwe(
+        code,
+        "algebra_ext",
+    )
+    tracer = Tracer(mwe)
+
+    tracer.load_trace_result(
+        os.path.join(script_dir, "../data/tracing_results/Algebra.Basics.Main.ast.json")
+    )
+    # Get tactics
+    code_bytes = code.encode("utf-8")
+    tactics = tracer.get_traced_tactic(tracer.tracing_result.tatics)
+
+    # Check tactics in gym
+    tactic = tactics[-1]
+    logger.info(f"tactic: {tactic.get_syntax_of_tactic(code_bytes)}")
+
+    # For some reason the following does not work:
+    # But running all tactics one by one works:
+    # {"sid": 0, "cmd": ["replace h : P.toRingHom = Q.toRingHom := FunLike.ext _ _ h", "have h' : (haveI := P; (\u00b7 \u2022 \u00b7) : R \u2192 A \u2192 A) = (haveI := Q; (\u00b7 \u2022 \u00b7) : R \u2192 A \u2192 A) := by\n    funext r a\n    rw [P.smul_def', Q.smul_def', h]", "rcases P with @\u27e8\u27e8P\u27e9\u27e9", "rcases Q with @\u27e8\u27e8Q\u27e9\u27e9"]}
+    # {"sid":0, "cmd":["  congr"]}
+    with Gym(mwe, tactic) as (gym, state_0):
+        state_1 = gym.run_tacs(state_0, [tactic.get_syntax_of_tactic(code_bytes)])
+        logger.debug(f"state_1: {state_1}")
+        assert not isinstance(state_1, ProofFinished)
