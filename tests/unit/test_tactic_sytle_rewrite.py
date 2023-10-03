c4f46f59cab83ee66634e108c8b2a5b1ceff2ba0
josojo <josojo@hotmail.de>
2023-10-03 16:10:04 +0200

    fix unit test

--- tests/unit/test_tactic_sytle_rewrite.py
+++ tests/unit/test_tactic_sytle_rewrite.py
@@ -23,6 +23,5 @@ def test_extracted_theorem_interaction_in_gym() -> None:
     )
     mwe.rewrite_to_tactic_style()
     theorem_string = """ by exact (
-  ⟨fun ⟨h⟩ => ⟨h⟩, fun ⟨h⟩ => ⟨h⟩⟩
-)"""
+  ⟨fun ⟨h⟩ => ⟨h⟩, fun ⟨h⟩ => ⟨h⟩⟩)\n"""
     assert mwe.code[mwe.proof_start : mwe.proof_end] == theorem_string
