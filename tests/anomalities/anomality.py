3f79769ac2d5d0562d2343047e2aa105dc26c5ba
josojo <josojo@hotmail.de>
2023-10-07 23:05:30 +0200

    fix lint

--- tests/anomalities/anomality.py
+++ tests/anomalities/anomality.py
@@ -8,19 +8,19 @@ from tests.utils.utils import read_code_from_file
 def test_extracted_theorem_interaction_in_gym() -> None:
     for i in range(0, 2):
         logger.debug(f"i: {i}")
-        code = read_code_from_file("./tests/data/Mathlib.Order.Bounds.Basics_rewrite.lean")
-
-        mwe = Mwe(
-            code,
-            "mem_lowerBounds_image",
-            i
+        code = read_code_from_file(
+            "./tests/data/Mathlib.Order.Bounds.Basics_rewrite.lean"
         )
+
+        mwe = Mwe(code, "mem_lowerBounds_image", i)
 
         tracer = Tracer(mwe)
         if i == 0:
             tracer.trace_mwe()
         else:
-            tracer.load_trace_result(tracer.execution_env.tmp_dir / "build/ir/Main.ast.json")
+            tracer.load_trace_result(
+                tracer.execution_env.tmp_dir / "build/ir/Main.ast.json"
+            )
         # Get tactics
         code_bytes = mwe.code.encode("utf-8")
         tactics = tracer.get_traced_tactic(tracer.tracing_result.tatics)
