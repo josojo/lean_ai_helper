6bc7e65e392fcc81982de1966d24ccd0f4aee495
josojo <josojo@hotmail.de>
2023-10-11 21:53:47 +0200

    testing

--- src/lean_env/setup.py
+++ src/lean_env/setup.py
@@ -3,7 +3,6 @@ import os
 from pathlib import Path
 from typing import Any, Dict, List, Protocol
 import ray
-from src.logger_config import logger
 
 
 class RayRemoteProtocol(Protocol):
@@ -25,9 +24,6 @@ class ParallelExecutor:
         results = []
         futures: Dict[Any, Path] = {}
         for argument in args:
-            logger.debug(f"argument: {argument}")
-            logger.debug(f"execution_envs_queue: {self.execution_envs_queue}")
-            logger.debug(f"futures: {self.execution_envs}")
             # Wait for an available folder
             while not self.execution_envs_queue:
                 # Check for completed tasks
@@ -41,8 +37,6 @@ class ParallelExecutor:
 
             # Start a task with an available folder
             env = self.execution_envs_queue.popleft()
-            logger.debug(f"env: {env}")
-            logger.debug(f"argument: {argument}")
             future = exec_function.remote(argument, env, self.output_dir)
             futures[future] = env
 
