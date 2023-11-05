9f1aae1f4ee910fb6740cce29d783e4454a3c3d4
josojo <josojo@hotmail.de>
2023-11-05 01:41:49 +0100

    lint

--- tests/e2e/test_tracing_mathlib4_example.py
+++ tests/e2e/test_tracing_mathlib4_example.py
@@ -31,4 +31,4 @@ def test_tracing_file() -> None:
     logger.debug(f"premise: {premise.pos.line}")
     assert premise.pos == PosEncoding(line=35, column=8)
     assert premise.end_pos == PosEncoding(line=35, column=22)
-    assert premise.def_pos == {'column': 8, 'line': 35}
+    assert premise.def_pos == {"column": 8, "line": 35}
